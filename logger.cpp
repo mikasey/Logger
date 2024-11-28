e80c2429d6f6779b54f2de4a4646abf1f9c9175e
Mikasey <mikaseysakugawa@gmail.com>
2024-11-28 22:25:40 +0200

    simpler - better

--- logger.cpp
+++ logger.cpp
@@ -1,72 +1,47 @@
-#include "logger.h"
+#include "logger.hpp"
 
+    int Logger::Init()
+    {
+        return 0;
+    }
 
+    const char* Logger::color_table[Logger::color_count] = {
+        "\x1b[30m", // black
+        "\x1b[31m", // red
+        "\x1b[32m", // green
+        "\x1b[33m", // yellow
+        "\x1b[34m", // blue
+        "\x1b[35m", // magenta
+        "\x1b[36m", // cyan
+        "\x1b[37m", // bright_gray
+        "\x1b[90m", // gray
+        "\x1b[91m", // bright_red
+        "\x1b[92m", // bright_green
+        "\x1b[93m", // bright_yellow
+        "\x1b[94m", // bright_blue
+        "\x1b[95m", // bright_magenta
+        "\x1b[96m", // bright_cyan
+        "\x1b[97m", // bright_white
+    };
 
-SFGE::Logger::Log_entry::Log_entry(int type, int sender, std::time_t time, std::string msg) :
-		_type(type), _sender(sender), _time(time), _msg(msg) {  }
+    const char* Logger::dt_format = "%d.%m.%y|%H:%M:%S";
+    const char* Logger::log_format = "%s[%s] %s: %s \033[0m\n";
+    const size_t Logger::max_msg_size = 1024;
+    const size_t Logger::max_time_str_size = 64;
 
-void SFGE::Logger::set_log_size(size_t new_size) {
-	// mayby check for max size, not sure
-	if (new_size >= _log_size) {
-		_log_size = new_size; //update array size
-	}
-	else {
-		// remove oldest elements that are not in bounds
-		_log_size = new_size; //update array size
-	}
-}
-size_t SFGE::Logger::get_log_size() const { return _log_size; }
+    void Logger::_log_console(const char* prefex, Color color, const char* msg_format, ...) {
+        char msg[max_msg_size];
+        char time_str[max_time_str_size];
+        time_t time_raw; time(&time_raw);
+        static struct tm time_tm = {};
+        localtime_s(&time_tm, &time_raw);
+        strftime(time_str, max_time_str_size, dt_format, &time_tm);
 
-SFGE::Logger::Logger(size_t log_size) {
-	_log_size = log_size;
-}
+        // Variable args magic
+        va_list args;
+        va_start(args, msg_format);
+        vsprintf_s(msg, msg_format, args);
+        va_end(args);
 
-void SFGE::Logger::add_entry(const int type, const int sender, const std::string msg) {
-	std::time_t time;
-	std::time(&time);
-	while (_log_queue.size() >= _log_size) {
-		_log_queue.pop_back();
-	}
-	_log_queue.emplace_front(type, sender, time, msg);
-
-}
-
-void SFGE::Logger::get_sorted_queue(std::vector<Log_entry>& sorted, std::function<bool(Log_entry, Log_entry)> comp) const {
-	sorted.reserve(_log_size);
-	for (Log_entry entry : _log_queue) {
-		sorted.push_back(entry);
-	}
-	std::sort(sorted.begin(), sorted.end(), comp);
-	return;
-}
-
-void SFGE::Logger::get_sorted_queue(std::vector<Log_entry>& sorting, const int bits_operation, const int bits_type, const int bits_sender ) const {
-	sorting.reserve(_log_size);
-	for (Log_entry entry : _log_queue) {
-		if((entry._type & bits_type) && (entry._sender & bits_sender))
-			sorting.push_back(entry);
-	}
-	std::function<bool(Log_entry, Log_entry)> compare_op;
-	switch (bits_operation) {
-		case OPERATION_LESS | SORT_BY_TIME:
-			compare_op = [&](Log_entry a, Log_entry b) -> bool { return a._time < b._time; };
-			break;
-		case OPERATION_LESS | SORT_BY_TYPE:
-			compare_op = [&](Log_entry a, Log_entry b) -> bool { return a._type < b._type; };
-			break;
-		case OPERATION_LESS | SORT_BY_SENDER:
-			compare_op = [&](Log_entry a, Log_entry b) -> bool { return a._sender < b._sender; };
-			break;
-		case OPERATION_MORE | SORT_BY_TIME:
-			compare_op = [&](Log_entry a, Log_entry b) -> bool { return a._time > b._time; };
-			break;
-		case OPERATION_MORE | SORT_BY_TYPE:
-			compare_op = [&](Log_entry a, Log_entry b) -> bool { return a._type > b._type; };
-			break;
-		case OPERATION_MORE | SORT_BY_SENDER:
-			compare_op = [&](Log_entry a, Log_entry b) -> bool { return a._sender > b._sender; };
-			break;
-	}
-	std::sort(sorting.begin(), sorting.end(), compare_op);
-	return;
-}
+        printf(log_format, color_table[color], time_str, prefex, msg);
+    }
--- main.cpp
+++ main.cpp
@@ -1,32 +1,12 @@
 #include <iostream>
 #include <thread>
 
-#include "logger.h"
+#include "logger.hpp"
 
-int main()
-{
-    using namespace SFGE;
-
-    Logger log(10);
-
-    log.add_entry(Logger::TYPE_DEBUG, Logger::SENDER_OS, "lol debug");
-    log.add_entry(Logger::TYPE_NOTIFICATION, Logger::SENDER_OS, "kek");
-    log.add_entry(Logger::TYPE_WARNING, Logger::SENDER_APP, "bruh");
-    log.add_entry(Logger::TYPE_DEBUG, Logger::SENDER_OPENGL, "debug");
-    log.add_entry(Logger::TYPE_NOTIFICATION, Logger::SENDER_OTHER, "idk");
-    log.add_entry(Logger::TYPE_WARNING, Logger::SENDER_APP, "sus");
-    log.add_entry(Logger::TYPE_DEBUG, Logger::SENDER_UNKNOWN, "??? debug?");
-    log.add_entry(Logger::TYPE_NOTIFICATION, Logger::SENDER_APP, "kek");
-    log.add_entry(Logger::TYPE_WARNING, Logger::SENDER_UNKNOWN, "sus");
-
-    std::vector<Logger::Log_entry> list;
-
-    auto sorting = [](Logger::Log_entry a, Logger::Log_entry b) -> bool { return a._sender > b._sender; };
-    log.get_sorted_queue(list, Logger::OPERATION_MORE | Logger::SORT_BY_TYPE, Logger::ALL_TRUE ^ Logger::TYPE_DEBUG, Logger::ALL_TRUE ^ Logger::SENDER_OTHER);
-
-    for (Logger::Log_entry msg : list) {
-        std::cout << "[" << msg._time << "]: \"" << msg._msg << "\" from " << msg._sender << std::endl;
-    }
+int main() {
+    LogInfo("Simple log here.");
+    LogWarning("Wait a sec...");
+    LogERROR("LMAO, %i", 69);
 
     std::cin.get();
     return 0;
